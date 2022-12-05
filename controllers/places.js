bb781691fc1fb69da0db34659d41c78a5f4c1bcf
kamel Barakat <kamelbarakat96@yahoo.com>
2022-12-05 01:40:41 -0500

    Part 9

--- controllers/places.js
+++ controllers/places.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const db = require('../models')
 
 router.get('/', (req, res) => {
-    db.Place.find()
+  db.Place.find()
     .then((places) => {
       res.render('places/index', { places })
     })
     .catch(err => {
-      console.log(err) 
+      console.log(err)
       res.render('error404')
     })
 })
@@ -15,14 +15,26 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
+    .then(() => {
       res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
+    })
+    .catch(err => {
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+        }
+        console.log('Validation error message', message)
+        res.render('places/new', { message })
+      }
+      else {
+        res.render('error404')
+      }
+    })
 })
+
+
 
 
 
@@ -51,7 +63,7 @@ router.post('/:id/rant', (req, res) => {
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+  res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function show(data) {
+function new_form (data) {
+  let message = ''                 
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
   return (
     <Def>
       <main>
         <h1>{data.place.name}</h1>
+        {message}
         <h2>Rating</h2>
         <img src={data.place.pic} alt={data.place.name} />
                 <h3>
@@ -14,6 +23,12 @@ function show(data) {
                 <h2>
                   Description
                 </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
                 <h2>Comments</h2>
         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit
